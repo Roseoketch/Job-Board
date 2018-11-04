56b7865eb9888fefa87bf96f39a49e14287e2068
John mutavi <jonnygovish@gmail.com>
2018-11-04 12:16:45 +0300

    Settings Add settings for django restframework

--- job/settings.py
+++ job/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'django_nose',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
